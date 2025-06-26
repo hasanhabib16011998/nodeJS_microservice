8110bdc6079072bf0082c93b540c3a00c42429fe
hasanhabib16011998 <hasan16habib@gmail.com>
2025-06-26 19:15:34 +0600

    updated code

--- project/post-service/src/utils/rabbitmq.js
+++ project/post-service/src/utils/rabbitmq.js
@@ -7,16 +7,21 @@ let channel = null;
 const EXCHANGE_NAME = "facebook_events";
 
 async function connectToRabbitMQ() {
-  try {
-    connection = await amqp.connect(process.env.RABBITMQ_URL);
-    channel = await connection.createChannel();
+  while (!connection){
+    try {
+      connection = await amqp.connect(process.env.RABBITMQ_URL);
+      channel = await connection.createChannel();
 
-    await channel.assertExchange(EXCHANGE_NAME, "topic", { durable: false });
-    logger.info("Connected to rabbit mq");
-    return channel;
-  } catch (e) {
-    logger.error("Error connecting to rabbit mq", e);
+      await channel.assertExchange(EXCHANGE_NAME, "topic", { durable: false });
+      logger.info("Connected to rabbit mq");
+      return channel;
+    } catch (e) {
+      logger.error("Error connecting to rabbit mq, retrying in 5s...", e);
+      await new Promise(res => setTimeout(res, 5000));
+    }
   }
+
+  return channel;
 }
 
 
